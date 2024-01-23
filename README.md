e8a5a432da515ac27ab54de04f9202d4238d20a5
AminSabet <39775559+aminsabetsarvestani@users.noreply.github.com>
2024-01-23 11:22:51 +0000

    Add files via upload
    Uploading final files

---
+++ README.md
@@ -0,0 +1,147 @@
+
+<p align="center">
+  <img src="Diagram_task.svg" alt="Alternative Text" width="700"/>
+</p>
+
+# Video Processing Pipeline Deployed on Ray/KubeRay Cluster
+
+#### This repository contains a AI video recognition pipeline designed to operate on a Ray/KubeRay cluster, leveraging the power of distributed computing for efficient handling and processing large number of video frames.
+
+The diagram above provides an overview of the designed pipeline, along with corresponding MLOps technologies. This pipeline is defined for a smart surveillance system and comprises four main components: producer, Kafka server, consumer, and model composition (Ray cluster). The following explains each component.
+
+There are up to 150 cameras transmitting HD-quality videos to the producer. The producer's role is to receive video frames and store them in a memory bucket, shared among different components in the pipeline. It extracts the name, path to the bucket, and camera ID for each frame, then sends this information as messages to topics in the Kafka server. There is a topic on the Kafka server for each camera, named from camera_1 to camera_150.
+
+To effectively handle the substantial volume of frames received from the cameras, the producer employs parallel processing using ray actors. Each actor is dedicated to processing video frames from a single camera. The efficiency and parallelization of the producer are further augmented by utilizing the async/wait functionality of Python. This approach allows for non-blocking communication with Kafka, ensuring that the rest of the code continues to execute uninterrupted. Each actor is capable of independently executing asynchronous operations. With multiple actors (one for each camera) running concurrently, each operates its own event loop, enabling simultaneous processing of multiple video streams. The process involves:
+
+1: Each actor retrieves frames from its respective camera.
+
+2: Frames are processed and prepared for transmission (e.g., storing in the bucket).
+
+3: Frame paths are sent to Kafka asynchronously.
+
+As these operations within each actor are asynchronous, there is no idle time while waiting for Kafka's I/O operations to finish.
+ 
+The Kafka server, used as a distributed event streaming platform, runs as a Docker image. There is a separate topic for each camera to manage them individually.
+
+The consumer mirrors the design of the producer, utilizing multiple parallel-running actors. Each actor within the consumer is linked to a specific Kafka topic corresponding to a camera. Similar to the producer, communication with the Kafka server in each actor is asynchronous. This design prevents any delay in receiving subsequent messages, thereby enhancing the consumer's efficiency. Each message received by the consumer includes the path to video frames (stored in the shared memory bucket) and the camera ID. Messages received by each actor are then formatted into HTTP requests and dispatched to a ray cluster. This setup ensures efficient handling of the data stream from each camera, maintaining the system's overall performance and responsiveness.
+
+The ray cluster, which contains several models including an inference service and multiple AI models, receives these requests. The inference service downloads the image from the bucket and sends it to multiple AI models for parallel processing. Notably, all parallel processing is designed in an asynchronous manner to avoid any bottlenecks in the system. The models run predictions on the images and save the results in a log file. Each model in the ray cluster has a queue for requests, an initial number of replicas, minimum and maximum replica counts, and a policy for starting new replicas. When the number of requests in the queue of each replica exceeds a preset value, a new replica of that model is started to address requests more quickly. Additionally, dynamic batch processing is added to each model, allowing a batch of images to be processed for maximum GPU utilization.
+
+Depending on the GPU infrastructure, the ray cluster configuration can be adjusted for maximum request handling. All producers, consumers, and the ray cluster can be executed either on a ray engine on a single-node, multiple-GPU infrastructure, or, for a multi-node, multi-GPU setup, the pipeline can be submitted (without any changes) as a job to a Kubernetes/KubeRay cluster.
+
+Notably, individual GPU and CPU resources are shared among different models, multiple replicas of each model, and actors, through the configuration of the cluster. This allows for maximum utilization of infrastructure via resource sharing.
+
+
+
+## Requirements
+### Hardware
+GPU: A GPU with a minimum of 10 GB memory is required to deploy the machine learning (ML) service. For scaling the application, the GPU requirement can increase up to 40 GB.
+### Software
+Docker for GPU: You will need to install Docker with GPU support to run containers that utilize GPU resources.
+Virtual Environment: Set up a Python virtual environment and install the required packages for isolated Python package management.
+## Setup and Installation
+Pull the Code
+Clone the repository to your local machine or download the source code.
+
+## Install Dependencies
+Install Docker with GPU support as per your system's requirements [here](https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/index.html).
+
+Create a Python virtual environment and activate it.
+```
+virtualenv dev
+source dev/bin/activate
+```
+Install the required Python packages (ensure that you have a requirements.txt file in your project).
+```
+pip install -r requirements.txt
+```
+## Run Apache Kafka
+Apache Kafka is used as a distributed event store and for stream-processing. Follow these steps to set up Kafka:
+
+### Make the Initialization Script Executable
+Run the following command to make the Kafka initialization script executable:
+
+```
+chmod +x kafka-init.sh
+```
+
+This script is responsible for generating 150 topics named 'camera_1' to 'camera_150', which are used by the event producer and consumer components of the application.
+
+### Start the Kafka Server
+Use Docker Compose to start the Kafka server along with the 150 topics:
+
+**Note**: The creation of 150 topics may take several seconds.
+
+```
+sudo docker compose up -d
+```
+
+**Optional:** You can verify the creation of the topics by accessing the Kafka broker container and listing the topics:
+
+```
+docker exec -it broker /bin/sh
+```
+Then, execute the following command within the broker container to list all topics:
+
+```
+kafka-topics --list --bootstrap-server localhost:9092
+```
+
+## Run Ray Cluster
+
+The Ray cluster consists of a Kafka consumer service that receives events from the Kafka server. These events are then fed into ML models for processing by various image processing models.
+
+```
+python ray_cluster.py
+```
+
+Once the cluster is runing, you can run the event producer to feed the kafka server with event (video frames) and  they will be used by conseumer and consequently by my AI models. 
+
+**Note:**  Multiple cameras are needed to feed video frames to the producer. To simplify the system for testing, two video clips are used to simulate the incoming video from cameras. These videos are sampled at 30fps to mimic a real camera scenario. Only minimal modifications to the producer are required to enable it to read videos from cameras instead of clips.
+```
+python ray_producer.py
+```
+The prediction of AI models are stored in `ray_cluster.log`, created by the cluster.
+---
+
+## Cluster Configuration
+
+### `config.yaml` File
+
+This file contains the configuration settings for your Ray cluster. It allows you to define the specifications of your cluster, including types of the required resources (e.g. CPU/GPU), number of replicas for each AI models. Additionally, it includes policies for the cluster's autoscaling behavior. By fine-tuning these configurations, you can optimize the cluster's performance to suit your specific needs.
+
+
+
+#### Factors Influencing the Processing Rate
+
+The processing rate of AI models in this pipeline is influenced by several key factors:
+
+1. **GPU Capacity**: The ability of the GPU to accommodate the maximum number of AI model replicas.
+2. **Cluster Configuration**: The configuration of your cluster including, initial replica, max replica, and so on 
+2. **Batch Sizes**: The size of dynamic batches processed by the AI models.
+3. **Request Queue**: The number of ongoing requests per queue.
+
+By adjusting these parameters, you can significantly impact the frame rate per second achieved by your AI models.
+
+#### To measure the processing rate:
+
+1. Ensure your cluster is configured according to your requirements.
+2. Run the `send_request.py` script. This script sends concurrent requests to your cluster, simulating a real-world load.
+
+The script will log the processing rate, measured in responses per second, into the `responsetime.log` file. This log provides insights into the performance of your cluster under different configurations and workloads.
+
+With a 15 GB GPU memory and after experimenting with several configurations, I achieved a processing rate of 230 frames per second using the ray_cluster, which operates two image classifiers (ResNet18) in parallel. Higher FPS can be achieved by utilizing a high-performance infrastructure and optimal configuration of the cluster.
+
+## To Do:
+Packaging and Containerization of Ray Producer and Service Deployment.
+
+Improve time to deployment using Ray model store. 
+## The following video demonstrates a test of the pipeline's autoscaling capabilities when the producer component sends a large number of requests.
+
+
+
+
+
+
+https://github.com/amnescher/VideoStreaming/assets/112868804/a6cc4338-734d-4d23-bf45-1ad177f726ce
+
---
+++ ray_cluster.py
@@ -0,0 +1,271 @@
+import requests
+from fastapi import FastAPI
+from ray import serve
+import logging
+import torch
+from torchvision import models, transforms
+from PIL import Image
+from typing import Optional, List
+from pydantic import BaseModel
+from ray.serve.handle import DeploymentHandle
+import ray
+import yaml
+import asyncio
+from aiokafka import AIOKafkaConsumer
+import logging
+import requests
+import ray
+
+class Config:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+with open("config.yaml", 'r') as file:
+    config = yaml.safe_load(file)
+    config = Config(**config)
+
+# Define the Input class
+class Input(BaseModel):
+    img_path: str
+    camera_id: Optional[str]
+
+
+
+# Configure logging
+log_file = 'kafka_consumer.log'
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s', filename=log_file, filemode='a')
+logging.getLogger('aiokafka').setLevel(logging.WARNING)
+
+'''
+This is a class definition for a RayConsumer, which is a remote class in the Ray framework. 
+consume: Asynchronously consumes messages from a Kafka server, processes the messages, and handles exceptions while consuming messages.'''
+@ray.remote
+class RayConsumer:
+    def __init__(self, topic, inference_engine_url):
+        self.inference_engine_url = inference_engine_url
+        self.topic = topic
+        self.healthy = True
+
+    async def consume(self):
+        loop = asyncio.get_running_loop()
+
+        self.consumer = AIOKafkaConsumer(
+            self.topic,
+            bootstrap_servers='localhost:29092',
+            group_id="ray-group",
+            loop=loop)
+        
+        try:
+            await self.consumer.start()
+            logging.info("Successfully connected to Kafka server")
+
+            async for msg in self.consumer:
+                logging.info(f"Consumed: {msg.topic}, {msg.partition}, {msg.offset}, {msg.key}, {msg.value}, {msg.timestamp}")
+                payload = {"img_path": msg.value.decode('utf-8'), "camera_id": msg.topic}
+                response = requests.post(self.inference_engine_url, json=payload)
+                logging.debug(f"Model output: {response.json()}")
+
+        except Exception as e:
+            logging.error(f"Error in message consumption: {e}")
+            self.healthy = False
+        finally:
+            await self.consumer.stop()
+            self.healthy = False
+            logging.info("Kafka Consumer stopped")
+
+
+async def run_consumer(topic, url):
+    consumer = RayConsumer.remote(topic, url)
+    await consumer.consume.remote()
+
+async def main():
+    topics = ["camera_1", "camera_2"]  # Add more topics as needed
+    inference_engine_url = "http://localhost:8000/inference"
+    tasks = [run_consumer(topic, inference_engine_url) for topic in topics]
+    await asyncio.gather(*tasks)
+
+
+# Define a FastAPI app and wrap it in a deployment with a route handler.
+app = FastAPI()
+
+
+
+# Define the Inference class
+@serve.deployment(autoscaling_config={
+        "min_replicas": config.inference["min_replicas"],
+        "initial_replicas": config.inference["initial_replicas"],
+        "max_replicas": config.inference["max_replicas"],
+        "target_num_ongoing_requests_per_replica": config.inference["target_num_ongoing_requests_per_replica"],
+        "graceful_shutdown_timeout_s": config.inference["graceful_shutdown_timeout_s"]}, route_prefix="/")
+@serve.ingress(app)
+class Inference:
+    def __init__(self, Classifier1: DeploymentHandle, Classifier2: DeploymentHandle,):
+        self.fastapi_deployment =  Classifier1
+        self.Classifier2 = Classifier2
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+            filename="ray_cluster.log",  # specify the file name if you want logging to be stored in a file
+            filemode="a",  # append to the log file if it exists
+        )
+        self.logger = logging.getLogger(__name__)
+        self.logger.propagate = True
+    # FastAPI will automatically parse the HTTP request for us.
+    @app.post("/inference")
+    async def base(self, request: Input):
+        try:
+            # returns a ray object that needs to be converted 
+            result1 = await self.fastapi_deployment.remote(request)
+            result2 = await self.Classifier2.remote(request)
+            return "success"
+        except Exception as e:
+            logging.error(f"Error in Inference service: {e}")
+            return {"error": str(e)}
+    
+
+
+
+@serve.deployment(ray_actor_options={"num_gpus": config.classifier1["num_gpus"]},autoscaling_config={
+        "min_replicas": config.classifier1["min_replicas"],
+        "initial_replicas": config.classifier1["initial_replicas"],
+        "max_replicas": config.classifier1["max_replicas"],
+        "target_num_ongoing_requests_per_replica": config.classifier1["target_num_ongoing_requests_per_replica"],
+        "graceful_shutdown_timeout_s": config.classifier1["graceful_shutdown_timeout_s"],})
+
+
+# Define the Classifier1 class
+class Classifier1:
+    def __init__(self):
+        # Configure logging to write to a file
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+            filename="ray_cluster.log",  # specify the file name if you want logging to be stored in a file
+            filemode="a",  # append to the log file if it exists
+        )
+        self.logger = logging.getLogger(__name__)
+        self.logger.propagate = True
+        self.logger.info("Initializing model 1 ...")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.logger.info(f"Using device: {self.device}")
+        self.model = models.resnet18(pretrained=True)
+        self.model.eval()
+        self.model.to(self.device)
+        self.transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+        
+        self.logger.info("Model 1 initialized")
+    @serve.batch(max_batch_size=config.classifier1["max_batch_size"],batch_wait_timeout_s=config.classifier1["batch_wait_timeout_s"]) 
+    async def handle_batch(self, requests: List[Input]):
+        inputs = []
+        camera_ids = []
+        for request in requests:
+            try:
+                image = Image.open(request.img_path)
+                transformed_image = self.transform(image)
+                inputs.append(transformed_image.to(self.device))  # Move tensor to the same device as model
+                camera_ids.append(request.camera_id)  # Store camera_id for logging
+            except Exception as e:
+                self.logger.error(f"Error processing image at path {request.img_path}: {e}")
+                inputs.append(torch.zeros([3, 224, 224], device=self.device))  # Placeholder for failed images
+                camera_ids.append(request.camera_id)  # Store camera_id for logging
+
+        inputs = torch.stack(inputs)
+        try:
+            with torch.no_grad():
+                predictions = self.model(inputs)
+                _, predicted = torch.max(predictions, 1)
+            # Log predictions with corresponding camera_ids
+            for camera_id, prediction in zip(camera_ids, predicted):
+                self.logger.info(f"Camera ID: {camera_id}, Prediction classifier 1: {prediction.item()}")
+            return [prediction.item() for prediction in predicted]
+        except Exception as e:
+            self.logger.error(f"Error during model prediction: {e}")
+            return ["Error" for _ in requests]
+
+    async def __call__(self, request: Input):
+        try:
+            return await self.handle_batch(request)
+        except Exception as e:
+            self.logger.error(f"Error in base endpoint: {e}")
+            return {"error": str(e)}
+
+# Define the Classifier2 class  
+@serve.deployment(ray_actor_options={"num_gpus": config.classifier2["num_gpus"]},autoscaling_config={
+        "min_replicas": config.classifier2["min_replicas"],
+        "initial_replicas": config.classifier2["initial_replicas"],
+        "max_replicas": config.classifier2["max_replicas"],
+        "target_num_ongoing_requests_per_replica": config.classifier2["target_num_ongoing_requests_per_replica"],
+        "graceful_shutdown_timeout_s": config.classifier2["graceful_shutdown_timeout_s"],})
+class Classifier2:
+    def __init__(self):
+        # Configure logging to write to a file
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+            filename="ray_cluster.log",  # specify the file name if you want logging to be stored in a file
+            filemode="a",  # append to the log file if it exists
+        )
+        self.logger = logging.getLogger(__name__)
+        self.logger.propagate = True
+        self.logger.info("Initializing model 2...")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.logger.info(f"Using device: {self.device}")
+        self.model = models.resnet18(pretrained=True)
+        self.model.eval()
+        self.model.to(self.device)
+        self.transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+        
+        self.logger.info("Model 2 initialized")
+    @serve.batch(max_batch_size=config.classifier2["max_batch_size"],batch_wait_timeout_s=config.classifier2["batch_wait_timeout_s"])
+    async def handle_batch(self, requests: List[Input]):
+        inputs = []
+        camera_ids = []
+        for request in requests:
+            try:
+                image = Image.open(request.img_path)
+                transformed_image = self.transform(image)
+                inputs.append(transformed_image.to(self.device))  # Move tensor to the same device as model
+                camera_ids.append(request.camera_id)  # Store camera_id for logging
+            except Exception as e:
+                self.logger.error(f"Error processing image at path {request.img_path}: {e}")
+                inputs.append(torch.zeros([3, 224, 224], device=self.device))  # Placeholder for failed images
+                camera_ids.append(request.camera_id)  # Store camera_id for logging
+
+        inputs = torch.stack(inputs)
+        try:
+            with torch.no_grad():
+                predictions = self.model(inputs)
+                _, predicted = torch.max(predictions, 1)
+            # Log predictions with corresponding camera_ids
+            for camera_id, prediction in zip(camera_ids, predicted):
+                self.logger.info(f"Camera ID: {camera_id}, Prediction classifier 2: {prediction.item()}")
+            return [prediction.item() for prediction in predicted]
+        except Exception as e:
+            self.logger.error(f"Error during model prediction: {e}")
+            return ["Error" for _ in requests]
+
+    async def __call__(self, request: Input):
+        try:
+            
+            return await self.handle_batch(request)
+        except Exception as e:
+            self.logger.error(f"Error in base endpoint: {e}")
+            return {"error": str(e)}
+
+# Initialize Ray and Serve
+  
+ray.init() 
+serve.start()
+# define model combination and deployment
+serve.run(Inference.bind(Classifier1.bind(),Classifier2.bind()),name = "Inference")
+asyncio.run(main())
